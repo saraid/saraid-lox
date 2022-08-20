c2345d1bc6c4f06186204d495ebcc2657da16f76
Michael Chui <saraid216@gmail.com>
2022-08-19 21:59:51 -0700

    4.5

--- lib/saraid/lox/scanner.rb
+++ lib/saraid/lox/scanner.rb
@@ -24,6 +24,31 @@ module Saraid
       def is_at_end?
         current >= source.size
       end
+
+      def scan_token
+        c = advance
+        case c
+        when '(' then add_token :left_paren
+        when ')' then add_token :right_paren
+        when '{' then add_token :left_brace
+        when '}' then add_token :right_brace
+        when ',' then add_token :comma
+        when '.' then add_token :dot
+        when '-' then add_token :minus
+        when '+' then add_token :plus
+        when ';' then add_token :semicolon
+        when '*' then add_token :star
+        end
+      end
+
+      def advance
+        source[current+=1]
+      end
+
+      def add_token(type, literal = nil)
+        text = source[start, current]
+        tokens << Token.new(type, text, literal, line)
+      end
     end
   end
 end
